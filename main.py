3e255f649f8ff128301063bdc0ef8a656741a709
Kamran Feroz <kamranferoz@yahoo.com>
2023-08-31 23:13:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,29 @@ st.set_page_config(layout="wide", page_title="Image Background Remover")
 
 st.write("## Remove background from your image")
 st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
+    ":dog: Try uploading an image to watch the background magically removed. Full-quality images can be downloaded from the sidebar. "
 )
 st.sidebar.write("## Upload and download :gear:")
+
+
+# Include sidebar with credentials
+with st.sidebar:
+    st.markdown('Clean image background (V 0.1)')
+    st.markdown(""" 
+                #### Let's connect:
+                [Kamran Feroz](https://www.linkedin.com/in/kamranferoz/)
+
+                #### Powered by:
+                [OpenAI](https://openai.com/)
+                [Langchain](https://github.com/hwchase17/langchain)\n
+
+                #### Source code:
+                [Clean Image!](https://github.com/kamranferoz/bgRemoval)
+                """)
+st.markdown(
+    "<style>#MainMenu{visibility:hidden;}</style>",
+    unsafe_allow_html=True)
+
 
 MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
 
