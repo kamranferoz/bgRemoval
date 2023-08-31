9ace912b3170a55636e10e1c8c936e6ddbf92de2
Kamran Feroz <kamranferoz@yahoo.com>
2023-08-31 23:22:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ st.set_page_config(layout="wide", page_title="Image Background Remover")
 
 st.write("## Remove background from your image")
 st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full-quality images can be downloaded from the sidebar. "
+    "Try uploading an image to watch the background magically removed. Full-quality images can be downloaded from the sidebar. "
 )
 st.sidebar.write("## Upload and download :gear:")
 
