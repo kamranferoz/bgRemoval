11956b51b4119f6a6039ac6cff2e62af7b80ed6d
Kamran Feroz <kamranferoz@yahoo.com>
2023-09-01 16:42:56 +0500

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-# embed_component={'linkedin':"""<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-#                   <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="kamranferoz" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://pk.linkedin.com/in/kamranferoz?trk=profile-badge">Kamran Feroz</a></div>"""}
+embed_component = {'linkedin':"""<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+                  <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="kamranferoz" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://pk.linkedin.com/in/kamranferoz?trk=profile-badge">Kamran Feroz</a></div>"""}
 
-embed_component= {'linkedin':"""<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-                   <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="kamranferoz" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://pk.linkedin.com/in/kamranferoz?trk=profile-badge"></a></div>""", 'medium':"""<div style="overflow-y: scroll; height:500px;"> <div id="retainable-rss-embed" 
-data-rss="https://medium.com/feed/retainable,https://medium.com/feed/data-science-in-your-pocket"
-data-maxcols="3" 
-data-layout="grid"
-data-poststyle="inline" 
-data-readmore="Read the rest" 
-data-buttonclass="btn btn-primary" 
-data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""}
+# embed_component= {'linkedin':"""<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+#                    <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="kamranferoz" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://pk.linkedin.com/in/kamranferoz?trk=profile-badge"></a></div>""", 'medium':"""<div style="overflow-y: scroll; height:500px;"> <div id="retainable-rss-embed" 
+# data-rss="https://medium.com/feed/retainable,https://medium.com/feed/data-science-in-your-pocket"
+# data-maxcols="3" 
+# data-layout="grid"
+# data-poststyle="inline" 
+# data-readmore="Read the rest" 
+# data-buttonclass="btn btn-primary" 
+# data-offset="0"></div></div> <script src="https://www.twilik.com/assets/retainable/rss-embed/retainable-rss-embed.js"></script>"""}
