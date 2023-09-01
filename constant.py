62d61a875d508c5a8d9946fe31f3925522aaddf8
Kamran Feroz <kamranferoz@yahoo.com>
2023-09-01 15:34:06 +0500

    Update constant.py

--- constant.py
+++ constant.py
@@ -1,2 +1,7 @@
-embed_components={'linkedin':"""<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-                  <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="kamranferoz" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://pk.linkedin.com/in/kamranferoz?trk=profile-badge">Kamran Feroz</a></div>"""}
+import pandas as pd
+
+# embed_component={'linkedin':"""<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+#                   <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="kamranferoz" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://pk.linkedin.com/in/kamranferoz?trk=profile-badge">Kamran Feroz</a></div>"""}
+
+embed_component= {'linkedin':"""<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+        <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="mehulgupta7991" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/mehulgupta7991?trk=profile-badge"></a></div>""", 'medium':"""<div style="overflow-y: scroll; height:500px;"> <div id="retainable-rss-embed" 
