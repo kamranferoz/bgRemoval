325531503952c29718f62e4e1a72b3594587abaa
Kamran Feroz <kamranferoz@yahoo.com>
2023-09-01 15:19:00 +0500

    Create constant.py

---
+++ constant.py
@@ -0,0 +1,2 @@
+embed_components={'linkedin':"""<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+                  <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="kamranferoz" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://pk.linkedin.com/in/kamranferoz?trk=profile-badge">Kamran Feroz</a></div>"""}
